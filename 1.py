9d4a66745d023389efbae3e8286f255b66d77c0e
qijigaici <1904205383@qq.com>
2020-08-05 15:48:51 +0800

    1.0

---
+++ 1.py
@@ -0,0 +1,12 @@
+#计算生肖
+year = 2020
+
+shengxiao = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
+print(year % 12)
+print(shengxiao[year % 12])
+
+print('猪' in shengxiao)
+
+print(shengxiao * 4)
+print(list(filter(shengxiao (year % 12))))
+
---
+++ 3.py
@@ -0,0 +1,5 @@
+string = 'wsfdfsf'
+
+len(string)
+
+print(len(string))
